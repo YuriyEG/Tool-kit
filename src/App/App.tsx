51253b81ee16dcf6be2aa1348fb596f128264f43
YuriyEG <you@example.com>
2024-07-26 08:56:09 +1200

    create app.tsx

---
+++ src/App/App.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+  width: 100vw;
+  min-height: 100vh;
+  text-align: center;
+  background-color: rgb(223, 229, 206);
+`
+
+const App = () => {
+  return <Container></Container>
+}
+
+export default App
