530d6ec4fb7a5ab0c88e2b1822d148f833733f94
YuriyEG <you@example.com>
2024-07-26 21:27:48 +1200

    RepositoryItem add

--- src/widgets/RepositoryList.tsx
+++ src/widgets/RepositoryList.tsx
@@ -1,5 +1,13 @@
-const RepositoryList = () => {
-  return <ul></ul>
+import RepositoryItem from "../features/RepositoryItem"
+
+const RepositoryList = ({ list = [1, 2, 3, 4, 5] }) => {
+  return (
+    <ul>
+      {list.map(item => (
+        <RepositoryItem />
+      ))}
+    </ul>
+  )
 }
 
 export default RepositoryList
