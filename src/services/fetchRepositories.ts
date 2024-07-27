e6eaf341dd355299c3fd52a5c4ee89cbc70f9114
YuriyEG <you@example.com>
2024-07-28 03:14:14 +1200

    fetchRepositories 100%

--- src/services/fetchRepositories.ts
+++ src/services/fetchRepositories.ts
@@ -1,4 +1,5 @@
 async function fetchRepositories(query) {
+  console.log(query, "22222")
   const response = await fetch("https://api.github.com/graphql", {
     method: "POST",
     headers: {
@@ -8,7 +9,7 @@ async function fetchRepositories(query) {
     body: JSON.stringify({
       query: `
           query SearchRepositories($query: String!) {
-            search(query: $query, type: REPOSITORY, first: 10) {
+            search(query: $query, type: REPOSITORY, first: 100) {
               repositoryCount
               nodes {
                 ... on Repository {
@@ -18,6 +19,14 @@ async function fetchRepositories(query) {
                     login
                   }
                   url
+                  stargazerCount
+                            defaultBranchRef {
+                                    target {
+                                      ... on Commit {
+                                            committedDate
+                                    }
+                          }
+                    }
                 }
               }
             }
