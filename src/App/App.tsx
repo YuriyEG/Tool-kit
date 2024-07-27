fcf442d16f7e298c5650bae4eab38d4d7a1d79a2
YuriyEG <you@example.com>
2024-07-28 00:06:25 +1200

    App

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,27 +1,31 @@
+import { useState } from "react"
+
 import styled from "styled-components"
 
-import Pagination from "../shared/ui/pagination"
-import RepositoryCard from "../features/RepositoryCard"
-import { useState } from "react"
+import Pagination from "../shared/ui/Pagination"
+import SearchInput from "../shared/ui/SearchInput"
+import RepositoryList from "../widgets/RepositoryList"
 
 const Container = styled.div`
   width: 100vw;
   min-height: 100vh;
   text-align: center;
   background-color: rgb(223, 229, 206);
+  padding-top: 80px;
 `
 
 const App = () => {
   const [page, setPage] = useState(1)
   return (
     <Container>
+      <SearchInput searchHandler={v => console.log(v)} />
+      <RepositoryList />
       <Pagination
         totalItems={300}
         itemsPerPage={10}
         onPageChange={setPage}
         currentPage={page}
       />
-      <RepositoryCard />
     </Container>
   )
 }
