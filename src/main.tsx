00d9ec3dfcdec54fac2df5e0959ae4a9430a3070
YuriyEG <you@example.com>
2024-07-31 14:04:47 +1200

    `

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,6 @@ import FontStyles from "./styles/fontStyles"
 import { createRoot } from "react-dom/client"
 import App from "./App/App"
 
-import "./index.css"
-
 const container = document.getElementById("root")
 
 const root = createRoot(container)
